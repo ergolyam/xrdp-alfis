0167fe4c112c610f1f1d377887ff0cd373017a1b
ergolyam <grisha765@tuta.io>
2026-07-13 21:43:01 +0300

    [u] update preparation script

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 set -eu
 : "${USER:?env USER is not set}"
+: "${USER_UID:?env USER_UID is not set}"
+: "${USER_GID:?env USER_GID is not set}"
 
 if [ ! -e /data/alfis.toml ]; then
     echo "Creating /data/alfis.toml from default configuration"
     cp /usr/share/alfis/alfis.toml /data/alfis.toml
+    chown "${USER_UID}:${USER_GID}" /data/alfis.toml
 fi
 
-chown -R $USER:$USER /data
+[ "$(stat -c '%u:%g' /data)" = "${USER_UID}:${USER_GID}" ] ||
+    echo "Changing access rights for the /data directory"; chown -R "${USER_UID}:${USER_GID}" /data
