d277f93a2340bdb40884844e68d659a11309aa6e
ergolyam <grisha765@tuta.io>
2026-07-13 04:32:01 +0300

    [f] fix enable dark mode

--- rootfs/etc/profile.d/alfis-dark.sh
+++ rootfs/etc/profile.d/alfis-dark.sh
@@ -2,4 +2,6 @@
 
 if [ ! -f /tmp/darkmode ]; then
     sed -i 's#^dark_theme = true#dark_theme = false#' /data/alfis.toml
+else
+    sed -i 's#^dark_theme = false#dark_theme = true#' /data/alfis.toml
 fi
