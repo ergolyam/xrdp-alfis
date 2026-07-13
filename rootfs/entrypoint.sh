8e182257ba99db6327ccf5f8851bdfb71f23f20d
ergolyam <grisha765@tuta.io>
2026-07-13 21:52:59 +0300

    [f] fix preparation script

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 set -eu
-: "${USER:?env USER is not set}"
 : "${USER_UID:?env USER_UID is not set}"
 : "${USER_GID:?env USER_GID is not set}"
 
-if [ ! -e /data/alfis.toml ]; then
-    echo "Creating /data/alfis.toml from default configuration"
-    cp /usr/share/alfis/alfis.toml /data/alfis.toml
-    chown "${USER_UID}:${USER_GID}" /data/alfis.toml
+DATA_DIR="/data"
+
+if [ ! -e "${DATA_DIR}/alfis.toml" ]; then
+    echo "Creating ${DATA_DIR}/alfis.toml from default configuration"
+    cp /usr/share/alfis/alfis.toml "${DATA_DIR}/alfis.toml"
+    chown "${USER_UID}:${USER_GID}" "${DATA_DIR}/alfis.toml"
 fi
 
-[ "$(stat -c '%u:%g' /data)" = "${USER_UID}:${USER_GID}" ] ||
-    echo "Changing access rights for the /data directory"; chown -R "${USER_UID}:${USER_GID}" /data
+if [ "$(stat -c '%u:%g' "${DATA_DIR}")" != "${USER_UID}:${USER_GID}" ]; then
+    echo "Changing access rights for the ${DATA_DIR} directory"
+    chown -R "${USER_UID}:${USER_GID}" "${DATA_DIR}"
+fi
